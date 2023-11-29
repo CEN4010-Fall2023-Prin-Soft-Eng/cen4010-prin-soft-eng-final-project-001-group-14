293a8366c5b53775970e33e0119ca6c23b9b3535
iblank01 <ianblankenship32@gmail.com>
2023-11-28 20:57:37 -0500

    Updating schema to accept all of the data pulled from the PetFinder API

--- dog_db.js
+++ dog_db.js
@@ -1,14 +1,52 @@
 const mongoose = require('mongoose');
 
-// dog schema for database
+// Enhanced dog schema for database
 const dogSchema = new mongoose.Schema({
     dogId: { type: String, required: true, unique: true }, // ID from Petfinder
     name: String,
-    breed: String,
+    breed: {
+        primary: String,
+        secondary: String,
+        mixed: Boolean,
+        unknown: Boolean
+    },
     age: String,
     gender: String,
     size: String,
-    color: String,
+    color: {
+        primary: String,
+        secondary: String,
+        tertiary: String
+    },
+    coat: String,
+    attributes: {
+        spayed_neutered: Boolean,
+        house_trained: Boolean,
+        declawed: Boolean,
+        special_needs: Boolean,
+        shots_current: Boolean
+    },
+    environment: {
+        children: Boolean,
+        dogs: Boolean,
+        cats: Boolean
+    },
+    tags: [String],
+    description: String,
+    photos: [{
+        small: String,
+        medium: String,
+        large: String,
+        full: String
+    }],
+    videos: [{
+        embed: String
+    }],
+    status: String,
+    status_changed_at: Date,
+    published_at: Date,
+    organization_id: String,
+    url: String
 });
 
 // dog model for database
